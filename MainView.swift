8ad35d92fa94e5d9e1294a1d7a036331c5dc7b6b
tanya <tlathion@icloud.com>
2024-05-19 21:48:46 +0200

    📚An exercice about Stacks

--- MainView.swift
+++ MainView.swift
@@ -9,7 +9,23 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                ZStack {
+                    HStack(spacing: 8) {
+                        Text("Demo").scaledToFit().frame(
+                            width: 100, height: 100, alignment: .center)
+                        VStack(spacing: 10) {
+                            Text("Little Lemon").font(.title)
+                            Text("Tomato Tortellini, Bottarga and Carbonara ").font(.title3)
+                                .multilineTextAlignment(.center)
+                        }// :- VStack
+                            .padding()
+                    }  // :- HStack
+                    .padding()
+                }  // :- ZStack
+            }
+        }
     }
 }
 
