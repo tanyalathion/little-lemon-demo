9db3725ab25373ad634027be1a19d81dfbde0503
tanya <tlathion@icloud.com>
2024-05-27 00:17:17 +0200

    Date picker exercice

---
+++ ReservationCalendar.swift
@@ -0,0 +1,18 @@
+//
+//  ReservationCalendar.swift
+//  Little Lemon Demo
+//
+//  Created by Tanya Lathion on 27.05.2024.
+//
+
+import SwiftUI
+
+struct ReservationCalendar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReservationCalendar()
+}
