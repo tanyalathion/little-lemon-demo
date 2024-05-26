e7203d9f51241567845b2560046baa4c0888cb83
tanya <tlathion@icloud.com>
2024-05-27 00:20:15 +0200

    comments on exercice

---
+++ DatePicker.swift
@@ -0,0 +1,18 @@
+//
+//  DatePicker.swift
+//  Little Lemon Demo
+//
+//  Created by Tanya Lathion on 27.05.2024.
+//
+
+import SwiftUI
+
+struct DatePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DatePicker()
+}
--- ReservationCalendar.swift
+++ ReservationCalendar.swift
@@ -3,13 +3,31 @@
 //  Little Lemon Demo
 //
 //  Created by Tanya Lathion on 27.05.2024.
-//
+//  Imaginez que vous êtes en train de créer une application de restaurant pour Little Lemon qui permet de faire des réservations. A un moment donné du processus de réservation, l'application doit demander aux utilisateurs de choisir une heure. Dans cet exercice, vous allez créer une application qui vous permet de sélectionner une heure et un jour sur le calendrier pour une réservation.
 
 import SwiftUI
 
 struct ReservationCalendar: View {
+    
+    @State var reservationDate = Date()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            HStack {
+                DatePicker(
+                    selection: $reservationDate, in: Date()...,
+                    displayedComponents: [.date, .hourAndMinute]
+                ) {}
+                Button(action: { print("do something!") }) {
+                    HStack {
+                        Image(systemName: "arrow.right.circle")
+                        Text("Done")
+                    }
+                }
+                .padding(20)
+                Text("Date is \(reservationDate.formatted(date: .long, time: .complete))")
+            }
+        }
     }
 }
 
