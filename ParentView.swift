33c6510c06f9ab179d007aeca4af433191c3df59
tanya <tlathion@icloud.com>
2024-05-28 22:38:22 +0200

    Cleaning view for exercice

--- ParentView.swift
+++ ParentView.swift
@@ -9,7 +9,20 @@ import SwiftUI
 
 struct ParentView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Rectangle()
+                .fill(Color.yellow)
+                .frame(width: 50, height: 50)
+            
+            Text("Rectangle One")
+                .foregroundColor(.white)
+                .padding(20)
+                .background(Color.yellow)
+            
+            Rectangle()
+                .fill(Color.blue)
+                .frame(width: 100, height: 30)
+        }
     }
 }
 
