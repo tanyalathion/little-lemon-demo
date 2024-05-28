0eb6a711270c9cb7a50b2c7bb34d6c312457b1cb
tanya <tlathion@icloud.com>
2024-05-28 22:36:33 +0200

    Exercice on views hierarchy

---
+++ ParentView.swift
@@ -0,0 +1,18 @@
+//
+//  ParentView.swift
+//  Little Lemon Demo
+//
+//  Created by Tanya Lathion on 28.05.2024.
+//
+
+import SwiftUI
+
+struct ParentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ParentView()
+}
